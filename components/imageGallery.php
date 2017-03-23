ae3024867583f4cca2c2c0d229ed5c60fef37baf
Ivo Petkov <ivo@ivopetkov.com>
2017-03-23 15:17:41 +0200

    Added new gallery type called "grid".

--- components/imageGallery.php
+++ components/imageGallery.php
@@ -22,6 +22,14 @@ $domDocument = new IvoPetkov\HTML5DOMDocument();
 $domDocument->loadHTML($component->innerHTML);
 $files = $domDocument->querySelectorAll('file');
 
+$type = 'columns';
+$temp = (string) $component->type;
+if ($temp !== '') {
+    if (array_search($temp, ['columns', 'grid']) !== false) {
+        $type = $temp;
+    }
+}
+
 $onClick = 'fullscreen';
 $temp = (string) $component->onClick;
 if ($temp !== '') {
@@ -30,37 +38,10 @@ if ($temp !== '') {
     }
 }
 
-$imageAspectRatio = null;
-$temp = (string) $component->imageAspectRatio;
-if ($temp !== '') {
-    if (preg_match('/^[0-9\.]+:[0-9\.]+$/', $temp) === 1) {
-        $imageAspectRatio = $temp;
-    }
-}
-
 $imageLoadingBackground = null;
 $temp = (string) $component->imageLoadingBackground;
 if ($temp !== '') {
     $imageLoadingBackground = $temp;
-}
-
-$columnsCount = 'auto';
-$temp = (string) $component->columnsCount;
-if ($temp !== '') {
-    if (is_numeric($temp)) {
-        $temp = (int) $temp;
-        if ($temp >= 1 && $temp <= 20) {
-            $columnsCount = $temp;
-        }
-    }
-}
-
-$imageSize = 'medium';
-$temp = (string) $component->imageSize;
-if ($temp !== '') {
-    if (array_search($temp, ['tiny', 'small', 'medium', 'large', 'huge']) !== false) {
-        $imageSize = $temp;
-    }
 }
 
 $spacing = '0px';
@@ -107,52 +88,168 @@ if ($onClick === 'fullscreen') {
     }
 }
 
-$getColumnsStyle = function($columnsCount, $attributeSelector = '') use ($galleryID, $spacing) {
-    $result = '#' . $galleryID . $attributeSelector . '>div{vertical-align:top;display:inline-block;width:calc((100% - ' . $spacing . '*' . ($columnsCount - 1) . ')/' . $columnsCount . ');margin-right:' . $spacing . ';margin-top:' . $spacing . ';}';
-    $result .= '#' . $galleryID . $attributeSelector . '>div:nth-child(' . $columnsCount . 'n){margin-right:0;}';
-    for ($i = 1; $i <= $columnsCount; $i++) {
-        $result .= '#' . $galleryID . $attributeSelector . '>div:nth-child(' . $i . '){margin-top:0;}';
-    }
-    return $result;
-};
+$imageAspectRatio = null;
 
 $containerStyle = '';
-if (is_numeric($columnsCount)) { // Fixed columns count
-    $containerStyle .= $getColumnsStyle($columnsCount);
-    if ($columnsCount > 1) {
-        $hasElementID = true;
+if ($type === 'columns') {
+
+    $columnsCount = 'auto';
+    $temp = (string) $component->columnsCount;
+    if ($temp !== '') {
+        if (is_numeric($temp)) {
+            $temp = (int) $temp;
+            if ($temp >= 1 && $temp <= 20) {
+                $columnsCount = $temp;
+            }
+        }
     }
-} else { // Auto columns count
+
+    $imageSize = 'medium';
+    $temp = (string) $component->imageSize;
+    if ($temp !== '') {
+        if (array_search($temp, ['tiny', 'small', 'medium', 'large', 'huge']) !== false) {
+            $imageSize = $temp;
+        }
+    }
+
+    $temp = (string) $component->imageAspectRatio;
+    if ($temp !== '') {
+        if (preg_match('/^[0-9\.]+:[0-9\.]+$/', $temp) === 1) {
+            $imageAspectRatio = $temp;
+        }
+    }
+
+    $getColumnsStyle = function($columnsCount, $attributeSelector = '') use ($galleryID, $spacing) {
+        $result = '#' . $galleryID . $attributeSelector . '>div{vertical-align:top;display:inline-block;width:calc((100% - ' . $spacing . '*' . ($columnsCount - 1) . ')/' . $columnsCount . ');margin-right:' . $spacing . ';margin-top:' . $spacing . ';}';
+        $result .= '#' . $galleryID . $attributeSelector . '>div:nth-child(' . $columnsCount . 'n){margin-right:0;}';
+        for ($i = 1; $i <= $columnsCount; $i++) {
+            $result .= '#' . $galleryID . $attributeSelector . '>div:nth-child(' . $i . '){margin-top:0;}';
+        }
+        return $result;
+    };
+
+    if (is_numeric($columnsCount)) { // Fixed columns count
+        $containerStyle .= $getColumnsStyle($columnsCount);
+        if ($columnsCount > 1) {
+            $hasElementID = true;
+        }
+    } else { // Auto columns count
+        $hasResponsiveAttributes = true;
+        $hasElementID = true;
+        $imageWidthMultipliers = [
+            'tiny' => 1,
+            'small' => 2,
+            'medium' => 3,
+            'large' => 4,
+            'huge' => 5
+        ];
+        $imageWidthMultiplier = $imageWidthMultipliers[$imageSize];
+        $responsiveAttributes = [];
+        $responsiveAttributes[] = 'w<' . ($imageWidthMultiplier * 100) . '=>data-columns=1';
+        $responsiveAttributes[] = 'w>=' . ($imageWidthMultiplier * 100) . '&&w<' . ($imageWidthMultiplier * 100 * 2) . '=>data-columns=2';
+        $responsiveAttributes[] = 'w>=' . ($imageWidthMultiplier * 100 * 2) . '&&w<' . ($imageWidthMultiplier * 100 * 3) . '=>data-columns=3';
+        $responsiveAttributes[] = 'w>=' . ($imageWidthMultiplier * 100 * 3) . '&&w<' . ($imageWidthMultiplier * 100 * 4.3) . '=>data-columns=4';
+        $responsiveAttributes[] = 'w>=' . ($imageWidthMultiplier * 100 * 4.3) . '&&w<' . ($imageWidthMultiplier * 100 * 6) . '=>data-columns=5';
+        $responsiveAttributes[] = 'w>=' . ($imageWidthMultiplier * 100 * 6) . '=>data-columns=6';
+        $containerAttributes .= ' data-responsive-attributes="' . implode(',', $responsiveAttributes) . '"';
+
+        for ($i = 1; $i <= 6; $i++) {
+            $containerStyle .= $getColumnsStyle($i, '[data-columns="' . $i . '"]');
+        }
+    }
+} elseif ($type === 'grid') {
+
+    $imageSize = 'medium';
+    $maxHeights = [
+        'tiny' => 90,
+        'small' => 150,
+        'medium' => 220,
+        'large' => 300,
+        'huge' => 400
+    ];
+    $temp = (string) $component->imageSize;
+    if ($temp !== '') {
+        if (isset($maxHeights[$temp])) {
+            $imageSize = $temp;
+        }
+    }
+
     $hasResponsiveAttributes = true;
     $hasElementID = true;
-    $imageWidthMultipliers = [
-        'tiny' => 1,
-        'small' => 2,
-        'medium' => 3,
-        'large' => 4,
-        'huge' => 5
-    ];
-    $imageWidthMultiplier = $imageWidthMultipliers[$imageSize];
-    $responsiveAttributes = [];
-    $responsiveAttributes[] = 'w>=0&&w<' . ($imageWidthMultiplier * 100) . '=>data-columns=1';
-    $responsiveAttributes[] = 'w>=' . ($imageWidthMultiplier * 100) . '&&w<' . ($imageWidthMultiplier * 100 * 2) . '=>data-columns=2';
-    $responsiveAttributes[] = 'w>=' . ($imageWidthMultiplier * 100 * 2) . '&&w<' . ($imageWidthMultiplier * 100 * 3) . '=>data-columns=3';
-    $responsiveAttributes[] = 'w>=' . ($imageWidthMultiplier * 100 * 3) . '&&w<' . ($imageWidthMultiplier * 100 * 4.3) . '=>data-columns=4';
-    $responsiveAttributes[] = 'w>=' . ($imageWidthMultiplier * 100 * 4.3) . '&&w<' . ($imageWidthMultiplier * 100 * 6) . '=>data-columns=5';
-    $responsiveAttributes[] = 'w>=' . ($imageWidthMultiplier * 100 * 6) . '=>data-columns=6';
-    $containerAttributes .= ' data-responsive-attributes="' . implode(',', $responsiveAttributes) . '"';
-
-    for ($i = 1; $i <= 6; $i++) {
-        $containerStyle .= $getColumnsStyle($i, '[data-columns="' . $i . '"]');
+    $maxHeight = $maxHeights[$imageSize];
+    $filesSizes = [];
+    foreach ($files as $index => $file) {
+        $filename = (string) $file->getAttribute('filename');
+        try {
+            $filesSizes[$index] = $app->images->getSize($filename);
+        } catch (\Exception $e) {
+            if ($app->config->displayErrors) {
+                throw $e;
+            }
+            $filesSizes[$index] = [0, 0];
+        }
     }
+
+    $addFilesToRow = function($attributeSelector, $filesOnRow, $isLastRow) use ($galleryID, &$containerStyle, $spacing) {
+        $totalWidth = array_sum($filesOnRow);
+        $counter = 0;
+        $filesCount = sizeof($filesOnRow);
+        foreach ($filesOnRow as $index => $width) {
+            $counter++;
+            $style = 'vertical-align:top;display:inline-block;width:calc((100% - ' . $spacing . '*' . ($filesCount - 1) . ')*' . (number_format($width / $totalWidth, 6, '.', '')) . ');';
+            if ($counter < $filesCount) {
+                $style .= 'margin-right:' . $spacing . ';';
+            }
+            if ($isLastRow) {
+                $style .= 'max-width:' . $width . 'px;';
+            } else {
+                $style .= 'margin-bottom:' . $spacing . ';';
+            }
+            $containerStyle .= '#' . $galleryID . $attributeSelector . '>div:nth-child(' . ($index + 1) . '){' . $style . '}';
+        }
+    };
+
+    $minGridImageWidth = 200;
+    $maxGridImageWidth = 2000;
+    $gridImageWidthStep = 200;
+
+    $responsiveAttributes = [];
+    for ($maxWidth = $minGridImageWidth; $maxWidth <= $maxGridImageWidth; $maxWidth += $gridImageWidthStep) {
+        $totalRowImagesWidth = 0;
+        $selector = '[data-grid="' . $maxWidth . '"]';
+        $filesOnRow = [];
+        $counter = 0;
+        foreach ($filesSizes as $index => $fileSize) {
+            $counter++;
+            list($fileWidth, $fileHeight) = $fileSize;
+            $maxFileWidth = $maxHeight / $fileHeight * $fileWidth;
+            if ($totalRowImagesWidth + $maxFileWidth > $maxWidth) {
+                if (!empty($filesOnRow)) {
+                    $addFilesToRow($selector, $filesOnRow, false);
+                }
+                $filesOnRow = [];
+                $totalRowImagesWidth = 0;
+            }
+            $filesOnRow[$index] = $maxFileWidth;
+            $totalRowImagesWidth += $maxFileWidth;
+        }
+        if (!empty($filesOnRow)) {
+            $addFilesToRow($selector, $filesOnRow, true);
+        }
+        if ($maxWidth - $gridImageWidthStep <= $minGridImageWidth) {
+            $responsiveAttributes['min'] = 'w<' . $maxWidth . '=>data-grid=' . $maxWidth;
+        } else {
+            $responsiveAttributes[] = 'w>=' . ($maxWidth - $gridImageWidthStep) . '&&w<' . ($maxWidth) . '=>data-grid=' . $maxWidth;
+        }
+    }
+    $responsiveAttributes[] = 'w>=' . $maxGridImageWidth . '=>data-grid=' . $maxGridImageWidth;
+    $containerAttributes .= ' data-responsive-attributes="' . implode(',', $responsiveAttributes) . '"';
 }
 
 $imageAttributes = '';
-
 if ($imageAspectRatio !== null) {
     $imageAttributes .= ' aspectRatio="' . $imageAspectRatio . '"';
 }
-
 if ($imageLoadingBackground !== null) {
     $imageAttributes .= ' loadingBackground="' . $imageLoadingBackground . '"';
 }
