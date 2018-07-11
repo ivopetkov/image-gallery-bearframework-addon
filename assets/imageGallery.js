76dcc466230185d2acba281e419f60cb800cbfcf
Ivo Petkov <ivo@ivopetkov.com>
2018-07-11 13:52:01 +0300

    Updated comments.

--- assets/imageGallery.js
+++ assets/imageGallery.js
@@ -1,7 +1,7 @@
 /*
  * Image gallery addon for Bear Framework
  * https://github.com/ivopetkov/image-gallery-bearframework-addon
- * Copyright (c) 2016 Ivo Petkov
+ * Copyright (c) Ivo Petkov
  * Free to use under the MIT license.
  */
 
