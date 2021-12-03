3a7b139f1291e1955a3c1d2f067cd402b3f9a203
Ivo Petkov <ivo@ivopetkov.com>
2021-12-03 09:22:23 +0200

    Update ImageGalleryTest.php

--- tests/ImageGalleryTest.php
+++ tests/ImageGalleryTest.php
@@ -19,9 +19,9 @@ class ImageGalleryTest extends BearFramework\AddonTests\PHPUnitTestCase
     public function testOutput()
     {
         $app = $this->getApp();
-        
+
         $tempDir = $this->getTempDir();
-        
+
         $this->makeSampleFile($tempDir . '/images/test1.jpg', 'jpg');
         $this->makeSampleFile($tempDir . '/images/test2.jpg', 'jpg');
         $this->makeSampleFile($tempDir . '/images/test3.jpg', 'jpg');
@@ -29,13 +29,12 @@ class ImageGalleryTest extends BearFramework\AddonTests\PHPUnitTestCase
         $app->assets->addDir($tempDir . '/images/');
 
         $result = $app->components->process('<component src="image-gallery">'
-                . '<file class="test-class-1" filename="' . htmlentities($tempDir . '/images/test1.jpg') . '"/>'
-                . '<file class="test-class-1" filename="' . htmlentities($tempDir . '/images/test2.jpg') . '"/>'
-                . '<file class="test-class-1" filename="' . htmlentities($tempDir . '/images/test3.jpg') . '"/>'
-                . '</component>');
+            . '<file class="test-class-1" filename="' . htmlentities($tempDir . '/images/test1.jpg') . '"/>'
+            . '<file class="test-class-1" filename="' . htmlentities($tempDir . '/images/test2.jpg') . '"/>'
+            . '<file class="test-class-1" filename="' . htmlentities($tempDir . '/images/test3.jpg') . '"/>'
+            . '</component>');
         $this->assertTrue(strpos($result, '/test1.jpg') !== false);
         $this->assertTrue(strpos($result, '/test2.jpg') !== false);
         $this->assertTrue(strpos($result, '/test3.jpg') !== false);
     }
-
 }
